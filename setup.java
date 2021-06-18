116727ecac0719b4fbd37717bd491a901105030c
Max <MaxThakurCodes@gmail.com>
2021-06-18 10:06:44 -0700

    Added Auth to setup

--- setup.java
+++ setup.java
@@ -1,3 +1,19 @@
 import dort.Utils;
+import Player;
+import Client;
+import Minecraft
+
+
+this.player = new Player();
+this.client = new Client();
+this.cracked = false;
+this.auth = "github";
+this.authURL = "https://github.com/CorrosionClient/abcdefghijklmnopqrstuvwxyz1234567890/master/hwids.txt";
+public static String HWID = "HWID";
+public static Boolean Authed = true;
+public static Minecraft Mc = null;
+Mc = new Minecraft();
+
+
 
 dort.Utils.loadClient();
